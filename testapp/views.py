05821a3028672b65a0da1956d7ce4148fc926e72
sklearn21 <sklearn21@gmail.com>
2021-03-13 14:20:31 +0530

    complete.

--- testapp/views.py
+++ testapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def count_view(request):
+    count = int(request.COOKIES.get('count', 0))
+    new_count = count + 1
+    response = render(request, 'testapp/count.html', {'count': new_count})
+    response.set_cookie('count', new_count)
+    return response
